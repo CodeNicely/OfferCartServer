1324bc0f270ba941d3d3204f609c939fd5c7e098
Aditya <aditya999123@gmail.com>
2016-11-05 11:57:25 +0530

    mysql

--- offercart/settings.py
+++ offercart/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'offercart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'xendroiders$django1',
-        'USER': 'xendroiders',
-        'PASSWORD': 'adityaagr',
-        'HOST': 'xendroiders.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'xendroiders$django1',
+#         'USER': 'xendroiders',
+#         'PASSWORD': 'adityaagr',
+#         'HOST': 'xendroiders.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
