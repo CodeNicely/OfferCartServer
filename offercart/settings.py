cb514fb728e8eedbf4cbf20dcb78cf8187f1531f
Aditya <aditya999123@gmail.com>
2016-11-05 12:08:36 +0530

    mysql testing

--- offercart/settings.py
+++ offercart/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'offercart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'xendroiders$django2',
+        'NAME': 'xendroiders$default',
         'USER': 'xendroiders',
         'PASSWORD': 'adityaagr',
         'HOST': 'xendroiders.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
