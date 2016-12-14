129057e4855631e28fc268f996cb74ea99670880
Meghal <meghal.nit@gmail.com>
2016-12-15 03:10:44 +0530

    Db user changed

--- offercart/settings.py
+++ offercart/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'discountstore',
-        'USER': 'meghal',
+        'USER': 'root',
         'PASSWORD': 'arjunpandey',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
