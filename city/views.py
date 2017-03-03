1bfffd3ad8e13723356347bc8f2f7c6e35a88681
ramya <educate1996@gmail.com>
2017-03-03 22:10:54 +0530

    improved foregraound notifications:working fine

--- city/views.py
+++ city/views.py
@@ -44,7 +44,7 @@ def city(request):
 			json=jwt.decode(str(access_token),'999123',algorithms='HS256')
 			print"debuuged 43"
 			user_list=user_data.objects.get(mobile=str(json['mobile']))
-			city_fcm=city_fcm_data.objects.create(fcm=fcm_city,city_id=city_id,user_id=str(json['mobile']))
+			city_fcm,created=city_fcm_data.objects.get_or_create(fcm=fcm_city,city_id=city_id,user_id=str(json['mobile']))
 			print"debuuged 45"
 			setattr(user_list,'city',int(city_id))
 			user_list.save()
--- notification/views.py
+++ notification/views.py
@@ -3,29 +3,40 @@ from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
 import requests
+import json
 from city.models import city_fcm_data
+from shop.models import shop_data
 
 # Create your views here.
 @csrf_exempt
 def send_notification(request):
+	#response_json={}
 	if request.method=='GET':
 		return render(request,"notification.html",{})
 	else:
 		for x,y in request.POST.items():
 			print "key,value",x,":",y
-		title=str(request.POST.get('title'))
 		message=str(request.POST.get('message'))
 		city=request.POST['city']
-		#intent_type=int(request.POST['category'])
-		#l=login_data.objects.create(name=name,password=password,gender=gender,email=email,description=description,city=city,mobile=mobile)
-		
+		shop_id=request.POST['shop_id']
+		# shop=category_data.objects.values('id', 'name').exclude(name__isnull=True)
+		# intent_type=int(request.POST['category'])
+		# for o in shop:
+		# 	shop_list=[]
+		# 	shop_list_data={}
+		# 	shop_list_data["shop_id"]=shop.id;
+		# 	shop_list_data["shop_name"]=shop.name;
+		# 	shop_list.append(shop_list_data)
+		# 	response_json["shop_data"]=shop_list
 		for o in city_fcm_data.objects.filter(city_id=city):
-			notify_users(o.fcm,""+title+""+message)
+			notify_users(o.fcm,message,shop_id)
+		# print(response_json)
+		# #obj_as_json":json.dumps(response_json)
 		return render(request,"notification.html",{}) 
 
 
 @csrf_exempt
-def notify_users(fcm,body,title="Discount Store"):
+def notify_users(fcm,body,data,title="Discount Store"):
 	json= {
 	"to" :str(fcm),
     "notification" : {
@@ -34,8 +45,10 @@ def notify_users(fcm,body,title="Discount Store"):
       "sound": "default",
     },
     "data" : {
-      "Nick" : str(body)+"",
-      "Room" : str(title),
+      "text":"Click to check new offers",
+      "message":str(body),
+      "shop_id":str(shop_id),
+      "shop_name":"Khushee Sarees",
     }
 	}
 	print json
