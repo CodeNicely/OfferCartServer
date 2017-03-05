06245d1f1d15668054b3525054dc096ce39a7821
ramya <educate1996@gmail.com>
2017-03-06 00:33:29 +0530

    changed db

--- offercart/settings.py
+++ offercart/settings.py
@@ -96,22 +96,22 @@ WSGI_APPLICATION = 'offercart.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'discountstore',
-#         'USER': 'root',
-#         'PASSWORD': 'arjunpandey',
-#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#         'PORT': '3306',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'discountstore',
+        'USER': 'root',
+        'PASSWORD': 'arjunpandey',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
