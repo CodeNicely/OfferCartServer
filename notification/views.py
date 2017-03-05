e8f14a5a170031eaecb47b19c70a54e9f54c28de
ramya <educate1996@gmail.com>
2017-03-05 20:22:29 +0530

    notification  panel working fine

--- notification/views.py
+++ notification/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import *
 import requests
 import json
@@ -26,14 +28,13 @@ def send_notification(request):
 		
 		for x,y in request.POST.items():
 			print "key,value",x,":",y
-		shops=shop_data.objects.values('id','name');
 		message=str(request.POST.get('message'))
 		city=request.POST['city']
-		shop_id=request.POST.get('id')
+		shop_id=request.POST.get('shops')
 		shop_name=str(shop_data.objects.get(id=shop_id).name)
 		for o in city_fcm_data.objects.filter(city_id=city):
 			notify_users(o.fcm,message,shop_id,shop_name)
-		return render(request,"notification.html",{"shop_data":shops}) 
+		return render(request,"notification.html",{}) 
 
 
 @csrf_exempt
@@ -63,3 +64,30 @@ def notify_users(fcm,body,id,name,title="Discount Store"):
 	r=requests.post(url,headers=headers,json=json)
 	for o in r:
 		print o
+
+@csrf_exempt
+def send_shops(request):
+	response_json={}
+	if (request.method=='POST'):
+		city_id=request.POST.get('city_id')
+		shop_names=shop_data.objects.filter(city_name__id=city_id)
+		response_json["shop_data"]=[]
+		if(shop_names.count()==0):
+			temp_json={}
+			temp_json["shop_id"]=""
+			temp_json["shop_name"]="No shop available"
+			response_json["shop_data"].append(temp_json)
+			print"debugged"
+		print"debugged"
+		for o in shop_names:
+			temp_json={}
+			print o.name
+			temp_json["shop_id"]=o.id
+			temp_json["shop_name"]=str(o.name)
+			response_json["shop_data"].append(temp_json)
+		print ("hmm",shop_names)
+		if(city_id!=0):
+			print ("ok",city_id)
+		print str(response_json)
+		return JsonResponse(response_json)
+
