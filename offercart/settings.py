bef9380efc4a4288d7f5b3a96e9dac733560d0c2
ujjwalagrawal17 <ujjwal.iitism@gmail.com>
2017-07-08 14:46:20 +0530

    Bugs Fixed

--- offercart/settings.py
+++ offercart/settings.py
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'offercart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'brandstoredb',
-#         'USER': 'brand_store_admin',
-#         'PASSWORD': 'cnadmin@999#',
-#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'brandstoredb',
+        'USER': 'brand_store_admin',
+        'PASSWORD': 'cnadmin@999#',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
 
 
 
