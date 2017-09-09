80431c784be128eeac5f1f957f50da626d6ed9f9
ujjwalagrawal17 <ujjwal.iitism@gmail.com>
2017-09-10 05:03:30 +0530

    Bug Fixes

--- shop/views.py
+++ shop/views.py
@@ -406,10 +406,8 @@ def get_shop_location(request):
     if request.method == 'GET':
         try:
             shop_access_token = str(request.GET.get('shop_access_token'))
-            print (shop_access_token)
             json = jwt.decode(str(shop_access_token), '810810', algorithms=['HS256'])
             shop_mobile = str(json['mobile'])
-            print (shop_mobile)
             try:
                 shop_instance = ShopData.objects.get(mobile=shop_mobile)
                 response['latitude'] = shop_instance.latitude
@@ -436,13 +434,11 @@ def change_shop_location(request):
     response = {}
     if request.method == 'GET':
         try:
-            shop_access_token = str(request.POST.get('shop_access_token'))
+            shop_access_token = str(request.GET.get('shop_access_token'))
             json = jwt.decode(str(shop_access_token), '810810', algorithms=['HS256'])
             shop_mobile = str(json['mobile'])
-
             latitude = str(request.GET.get('latitude'))
             longitude = str(request.GET.get('longitude'))
-
             try:
                 shop_instance = ShopData.objects.get(mobile=shop_mobile)
                 shop_instance.latitude = latitude
